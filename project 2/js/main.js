cfc5ccf2f5d0a806d331639cded310f0b60564db
digio <40724487+gioblog@users.noreply.github.com>
2025-11-21 22:39:13 -0500

    progress

--- project 2/js/main.js
+++ project 2/js/main.js
@@ -1,9 +1,9 @@
 //user clicks on search button 
-window.onload = (e) => {document.querySelector("#searchButton").onclick = searchButtonClicked}; 
+window.onload = (e) => {document.querySelector("#searchButton").onclick = SearchButtonClicked}; 
 
 let displayTerm = "";
 
-function searchButtonClicked()
+function SearchButtonClicked()
 {
     console.log("search button function started"); 
 
@@ -27,50 +27,66 @@ function searchButtonClicked()
     document.querySelector("#status").innerHTML = "Finding '" + displayTerm + "' broh"; 
 
     console.log(url); 
-    getData(url); 
+    GetData(url); 
 }
 
-function getData(url)
-{
+function GetData(url){
     let xhr = new XMLHttpRequest(); 
-    xhr.onload = dataLoaded; 
-    xhr.onerror = dataLoaded; 
+    // if(isEpisode)
+    // {
+    //     xhr.onload = EpisodeDataHandler(); 
+    //     xhr.onerror = EpisodeDataHandler(); 
+    // }
+
+    xhr.onload = DataLoaded; 
+    xhr.onerror = DataLoaded; 
 
     xhr.open("GET", url); 
     xhr.send(); 
 }
 
-function dataLoaded(e)
+function DataLoaded(e) 
 {
     let xhr = e.target; 
+    console.log("target" + e.target);  
     console.log(xhr.responseText); 
+    console.log(xhr.responseText.length); 
 
-    let obj = JSON.parse(xhr.responseText); 
-    if(!obj.data || obj.data.length == 0) //if there are no results, print a message and return 
+    let obj = JSON.parse(xhr.responseText); //doesn't work for other search terms 
+    console.log(obj); 
+    console.log(obj.results); 
+    //console.log(obj.data.length); 
+
+    if(!obj.results || obj.results.length == 0) //if there are no results, print a message and return 
 		{
 			document.querySelector("#status").innerHTML = "Can't find anything for '" + displayTerm + "' broh"
-			let nothingLine = `<div class='result'><img src='${media/cant-find.jpg}' />`; 
+			let nothingLine = `<div class='result'><img src='${"media/cant-find.jpg"}' />`; 
+            document.querySelector("#content").innerHTML = nothingLine; 
             return; 
 		}
 
 		//display to the user 
-		let results = obj.data; 
+		let results = obj.results; 
 		console.log("results.length = " + results.length); 
 		let bigString = ""; //HTML string for user result
 
 		for(let i = 0; i < results.length; i++)
 		{
 			let result = results[i]; 
-			let smallURL = result.image.fixed_width_downsampled.url; 
-			if(!smallURL) smalRL = "media/no-results.jpeg"; //when no result is found for the search 
-
-			let url = result.lUurl; //url to GIPHY page 
+			let smallURL = result.image; 
+		    //api comes with a default image if no image is avaliable 
 
 			//build div to hold the results - could also use concatenation, but that is messy 
 			let line = `<div class='result'><img src='${smallURL}' title= '${result.id}' />`; 
 			line += `<span><p> ${result.name}</p>`; 
-			line += `<p>Location: ${result.origin}</p></span>`;
-            line += `<p>Episode First Seen in: ${result.episode[1]} </p></div>`
+			line += `<p>Origin: ${result.origin.name}</p></span><div>`;
+
+            //trying to display episode title 
+
+            // let episodeURL = result.episode[0]; 
+            // console.log(episodeURL); 
+            // GetEpisodeData(episodeURL); //episodes are their own data set -- says the function is undefined??? 
+            // //line += `<p>Episode First Seen in: ${result.episode[0].name} </p></div>`
 
 			bigString += line; //store the HTML that is to be shown to the user 
 		}
@@ -78,11 +94,44 @@ function dataLoaded(e)
 		//updates the div section marked with the content id to display the search results 
 			//finds the first HTML element that matches the specified id and accesses the content with the element via the .innerHTML property 
 				//you can get or set the .innerHTML property 
-		document.querySelector("#content").innerHTML = bigString; 
+        let title = `<h2>Results</h2>`
+        document.querySelector("#content").innerHTML += title; 
+		document.querySelector("#content").innerHTML += bigString; 
 		document.querySelector("#status").innerHTML = "<b>Success!</b><p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>"; //updates the text below the search button 
 	}
++
+    function GetEpisodeData(url)
+    {
+        console.log("ive reached getepisodedata() !!"); 
+        let xhr = new XMLHttpRequest(); 
 
-	function dataError(e)
+        xhr.onload = EpisodeDataOutput(); 
+        xhr.onerror = EpisodeDataOutput(); 
+
+        xhr.open("GET", url); 
+        xhr.send(); 
+    }
+
+    function EpisodeDataOutput(e)
+    {
+        console.log("trying to display episode now"); 
+        let xhr = e.target; 
+        let firstEpisode = ""; 
+        console.log(xhr.responseText); 
+        console.log(xhr.responseText.length); 
+
+        let obj = JSON.parse(xhr.responseText); 
+        console.log(obj); 
+        console.log(obj.name); 
+        
+        console.log(firstEpisode); 
+        let line = `<div class='results'><span><p>First Seen In: ${obj.episode} ${obj.name}</p></span></div>`; 
+
+        document.querySelector("#content").innerHTML = line; 
+
+    }
+
+	function DataError(e)
 	{
 		console.log("an error occured"); 
 	}
