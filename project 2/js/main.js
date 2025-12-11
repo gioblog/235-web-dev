9babf7f3dc7dcf9909b1740db9e5830e5afb3a83
digio <40724487+gioblog@users.noreply.github.com>
2025-12-11 03:51:50 -0500

    so close - needs some css with the results a little

--- project 2/js/main.js
+++ project 2/js/main.js
@@ -1,21 +1,31 @@
 //user clicks on search button 
 window.onload = (e) => {document.querySelector("#searchButton").onclick = SearchButtonClicked}; 
 
+const searchBar = document.querySelector("#searchBar"); 
 let displayTerm = "";
-let lastSearch; //declare storage variable 
+
+//local storage 
+let lastSearch = "charSearched"; //declare storage variable 
+const storedTerm = localStorage.getItem(lastSearch); //stored data 
+if(storedTerm){
+    searchBar.value = storedTerm; 
+}else{
+    searchBar.value = "Rick";
+}
 
 function SearchButtonClicked()
 {
     console.log("search button function started"); 
 
-    const Rick_Morty_URL = "https://rickandmortyapi.com/api/character"; 
-    //can't find a api key hopefully don't need one 
+    //change CSS to account for search 
+    document.getElementById("searchFeature").style.margin = '2px 0px'; 
+   
 
+    const Rick_Morty_URL = "https://rickandmortyapi.com/api/character"; 
     let url = Rick_Morty_URL; 
 
     let searchTerm = document.querySelector("#searchBar").value; //grab user input 
     displayTerm = searchTerm; 
-    lastSearch = localStorage.getItem(displayTerm); //storing last term searched
 
     //parse user input 
     searchTerm = searchTerm.trim(); //get rid of leading & trailing spaces 
@@ -75,13 +85,41 @@ function DataLoaded(e)
 		for(let i = 0; i < results.length; i++)
 		{
 			let result = results[i]; 
+            console.log(document.getElementById("hideDefaultImg").checked); 
 			let smallURL = result.image; 
 		    //api comes with a default image if no image is avaliable 
 
+            let userChoice = document.getElementById("filterList").selectedOptions;
+            console.log(userChoice); 
+            let valuesCollected = Array.from(userChoice).map(({value}) => value); 
 			//build div to hold the results
 			let line = `<div class='result'><img src='${smallURL}' title= '${result.id}' />`; 
-			line += `<span><p> ${result.name}</p>`; 
-			line += `<p>Origin: ${result.origin.name}</p></span></div>`;
+            line += `<span><p> ${result.name}</p>`; 
+            if(valuesCollected.length > 0){
+                for(let i = 0; i < valuesCollected.length; i++){
+                    if(valuesCollected[i] == "life"){
+                        line += `<p> Status: ${result.status}</p>`;
+                    }
+
+                    if(valuesCollected[i] == "origins"){
+                        line += `<p>Origin: ${result.origin.name}</p>`;
+                    }
+
+                    if(valuesCollected[i] == "species"){
+                        line += `<p>Species: ${result.species}</p>`; 
+                    }
+                }
+            }
+
+            line += `</span></div>`; 
+
+            //don't display anything for this char if the user only wants to see chars with pictures 
+            if(result.image == "https://rickandmortyapi.com/api/character/avatar/19.jpeg" && 
+                document.getElementById("hideDefaultImg").checked)
+            {
+               line = ``;  
+            }
+
             //trying to display episode title 
 
             // let episodeURL = result.episode[0]; 
@@ -101,42 +139,45 @@ function DataLoaded(e)
 		document.querySelector("#status").innerHTML = "<b>Success!</b><p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>"; //updates the text below the search button 
 
         //local storage 
-        console.log(lastSearch); 
+        console.log(storedTerm); 
 	}
 +
-    function GetEpisodeData(url)
-    {
-        console.log("ive reached getepisodedata() !!"); 
-        let xhr = new XMLHttpRequest(); 
+    // function GetEpisodeData(url)
+    // {
+    //     console.log("ive reached getepisodedata() !!"); 
+    //     let xhr = new XMLHttpRequest(); 
 
-        xhr.onload = EpisodeDataOutput(); 
-        xhr.onerror = EpisodeDataOutput(); 
+    //     xhr.onload = EpisodeDataOutput(); 
+    //     xhr.onerror = EpisodeDataOutput(); 
 
-        xhr.open("GET", url); 
-        xhr.send(); 
-    }
+    //     xhr.open("GET", url); 
+    //     xhr.send(); 
+    // }
 
-    function EpisodeDataOutput(e)
-    {
-        console.log("trying to display episode now"); 
-        let xhr = e.target; 
-        let firstEpisode = ""; 
-        console.log(xhr.responseText); 
-        console.log(xhr.responseText.length); 
+    // function EpisodeDataOutput(e)
+    // {
+    //     console.log("trying to display episode now"); 
+    //     let xhr = e.target; 
+    //     let firstEpisode = ""; 
+    //     console.log(xhr.responseText); 
+    //     console.log(xhr.responseText.length); 
 
-        let obj = JSON.parse(xhr.responseText); 
-        console.log(obj); 
-        console.log(obj.name); 
+    //     let obj = JSON.parse(xhr.responseText); 
+    //     console.log(obj); 
+    //     console.log(obj.name); 
         
-        console.log(firstEpisode); 
-        let line = `<div class='results'><span><p>First Seen In: ${obj.episode} ${obj.name}</p></span></div>`; 
+    //     console.log(firstEpisode); 
+    //     let line = `<div class='results'><span><p>First Seen In: ${obj.episode} ${obj.name}</p></span></div>`; 
 
-        document.querySelector("#content").innerHTML = line; 
+    //     document.querySelector("#content").innerHTML = line; 
 
-    }
+    // }
 
 	function DataError(e)
 	{
 		console.log("an error occured"); 
 	}
+
+    //save of term from the serach bar every time a new one is typed 
+   searchBar.onchange = e=>{localStorage.setItem(lastSearch, e.target.value);};
 	
